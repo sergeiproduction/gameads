d09d2ec416821772f639b95f08887b3d99c953c7
sergeiproduction <sergei_9802@mail.ru>
2024-01-01 20:22:55 +0400

    Изменил переменную serverURL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { logout } from "./redux/slices/user";
 
-export const serverURL = "http://26.3.189.182:8000";
+// export const serverURL = "http://26.3.189.182:8000";
+export const serverURL = process.env.API_URL;
 
 const instance = axios.create({
   baseURL: serverURL,
